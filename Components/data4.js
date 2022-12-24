2a8107569ae77ff789046d12b3441d5776501b26
Payin Winful <fiifipayin@gmail.com>
2022-12-24 22:24:48 +0000

    history

---
+++ Components/data4.js
@@ -0,0 +1,69 @@
+const data4 = [
+    {
+        id: '1',
+        title: "Chris Maison",
+        price: 'GHc 93.00',
+        image: require('./img/chris.jpeg'),
+        time: '14 Dec, 2022 4:44PM Delivered',
+    },
+    {
+        id: '2',
+        title: "KFC Accra Mall",
+        price: 'GHc 66.20',
+        image: require('./img/kfc.jpeg'),
+        time: '30 May, 2022 5:44PM Delivered',
+    },
+    {
+        id: '3',
+        title: "Chris Maison",
+        price: 'GHc 42.40',
+        image: require('./img/chris.jpeg'),
+        time: '23 Feb, 2022 7:35PM Delivered',
+    },
+    {
+        id: '4',
+        title: "Chris Maison",
+        price: 'GHc 78.00',
+        image: require('./img/chris.jpeg'),
+        time: '02 Dec, 2022 5:02PM Delivered',
+    },
+    {
+        id: '5',
+        title: "KFC Accra Mall",
+        price: 'GHc 27.65',
+        image: require('./img/kfc.jpeg'),
+        time: '22 Nov, 2021 6:01PM Delivered',
+    },
+    {
+        id: '6',
+        title: 'Chic Kitchen',
+        price: 'GHc 71.40',
+        image: require('./img/chic.jpeg'),
+        time: '02 March, 2021 3:22PM Delivered',
+    },
+    {
+        id: '7',
+        title: "Munchy's East Legon",
+        price: 'GHc 92.10',
+        image: require('./img/munchy.jpeg'),
+        time: '14 Feb, 2021 7:42AM Delivered',
+    },
+    {
+        id: '8',
+        title: "Milady's Restaurant",
+        price: 'GHc 110.00',
+        image: require('./img/milady.jpeg'),
+        time: '9 Feb, 2021 11:22AM Delivered',
+    },
+    {
+        id: '9',
+        title: "Munchy's North Legon",
+        price: 'GHc 9.00',
+        image: require('./img/munchy.jpeg'),
+        time: '13 Jan, 2021 1:22PM Delivered',
+    },
+    
+    
+];
+
+export default data4;
--- screens/history.js
+++ screens/history.js
@@ -1,31 +1,94 @@
 import { StatusBar } from 'expo-status-bar';
-import { View, Text, StyleSheet, Dimensions, FlatList } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, FlatList, Image, ScrollView } from 'react-native'
 import React from 'react'
+import data4 from '../Components/data4';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 const Height = Dimensions.get('window').height;
 const Width = Dimensions.get('window').width;
 function History(){
   return (
-    <View>
+    <View style={styles.container}>
         <StatusBar style="dark" />
       <Text style={styles.order}>My orders</Text>
       <FlatList 
-      data={}
+      data={data4}
       keyExtractor = {(item)=> item.id}
       renderItem = {({item}) => (
-        <View></View>
+        <View style={styles.main}>
+          <View style={styles.list}>
+          <View style={styles.left}>
+            <Image style={styles.image} source={item.image}/>
+            <View style={styles.text1}>
+              <Text style={styles.text}>{item.title}</Text>
+              <Text style={styles.text}>{item.price}</Text>
+            </View>
+          </View>
+          <View style={styles.icon}>
+            <MaterialCommunityIcons name="restart" size={30} color="black" />
+          </View>
+        </View>
+        <Text style={styles.date}>{item.time}</Text>
+        </View>
       )}/>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
   order: {
     fontSize: 24,
     marginTop: Height * 0.09,
     fontWeight: 'bold',
     marginHorizontal:Width * 0.07,
+    marginVertical: Height * 0.01,
+  },
+  list : {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: -25
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 10,
+  },
+  left: {
+    flexDirection: 'row',
+  },
+  date: {
+    marginTop: Height * 0.015,
+    color: '#9F9F9C',
+    fontWeight: '400',
+    paddingBottom: 18
+  },
+  main: {
+    marginVertical: Height * 0.04,
+    borderBottomWidth: 1,
+    borderBottomColor: '#F5F5F5',
+    marginHorizontal:Width * 0.07,
+  },
+  text: {
+    marginLeft: Width * 0.04,
+    fontSize: 16,
+    fontWeight: '500'
+  },
+  text1: {
+    marginTop: Height * 0.015,
+  },
+  icon: {
+    marginTop: Height * 0.013,
+    paddingHorizontal: 9,
+    paddingVertical: 9,
+    backgroundColor: '#F5F5F5',
+    borderRadius: 60
   },
 })
 
